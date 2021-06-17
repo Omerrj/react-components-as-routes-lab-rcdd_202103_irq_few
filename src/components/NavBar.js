d151d1dec9b6827bb2e24ca771095b1040123896
Omer Sabah <omerrj99@gmail.com>
2021-06-17 00:43:09 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-           <NavLink to="/">Home</NavLink>
+      <NavLink to="/">Home</NavLink>
       <NavLink to="/movies">Movies</NavLink>
       <NavLink to="/directors">Directors</NavLink>
       <NavLink to="/actors">Actors</NavLink>
